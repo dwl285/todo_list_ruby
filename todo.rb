818883ea2b25e980ff8ee8908de8ad34e47da2ff
Daniel Lee <dwl285@Daniels-MacBook-Pro.local>
2017-10-06 09:14:10 +0100

    add status to tasks

--- todo.rb
+++ todo.rb
@@ -10,6 +10,7 @@ module Menu
       4) Show all tasks
       5) Write list to file
       6) Read list from file
+      7) Toggle a task's status
       Q) Quit the program\n"
   end
 
@@ -52,24 +53,30 @@ class List
     i = 1
     puts "Here's the contents of the list:"
     all_tasks.each {|t|
-      puts "#{i}) #{t.description}"
+      puts "#{i}) #{t.to_machine}"
       i+=1
     }
     print "\n"
+  end
+
+  def toggle(task_number)
+    all_tasks[task_number-1].toggle_status
   end
 
   def write_to_file(filename)
     File.new(filename, "w+")
     File.open(filename, "w+") do |f|
       all_tasks.each do |task|
-        f.puts(task.to_s)
+        f.puts(task.to_machine)
       end
     end
   end
 
   def read_from_file(filename)
     File.open(filename).each do |line|
-      all_tasks. << Task.new(line)
+      status = line[2]=="X"
+      description = line[6..line.length]
+      all_tasks << Task.new(description, status)
     end
   end
 
@@ -77,14 +84,39 @@ end
 
 class Task
   attr_reader :description
+  attr_accessor :status
 
-  def initialize(description)
+  def initialize(description, status=false)
     @description = description
+    @status = status
   end
 
   def to_s
     description.to_s
   end
+
+  def completed?
+    status
+  end
+
+  def to_machine
+    "#{represent_status} : #{to_s}"
+  end
+
+  def toggle_status
+    @status = !completed?
+  end
+
+  private
+
+  def represent_status
+    if completed?
+      return '[X]'
+    else
+      return '[ ]'
+    end
+  end
+
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -121,6 +153,11 @@ if __FILE__ == $PROGRAM_NAME
       filename = prompt("What's the filename of the list you'd like to read?")
       my_list.read_from_file(filename)
       puts "Todo list created from #{filename}"
+    when "7"
+      puts "Which task would you like to toggle the status of?"
+      task_number = prompt(my_list.show,"").to_i
+      my_list.toggle(task_number)
+      puts "Task toggled"
     else
       puts "Sorry, I didn't understand"
     end
